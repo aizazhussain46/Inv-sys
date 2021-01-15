b8b7cf7af911fa9506e8336964e5200a81dbc530
francisgill42 <francisgill42@gmail.com>
2021-01-15 12:26:11 +0500

    seeder,database fixes,user migration fixes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \DB::table('roles')->insert([
+            'role' => 'Admin'
+        ]);
+
+        \DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('secret') ,
+            'role_id' => 1
+        ]);
+
         // $this->call(UsersTableSeeder::class);
     }
 }
