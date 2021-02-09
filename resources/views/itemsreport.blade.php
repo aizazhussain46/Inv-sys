1550ea9d8a4ccdb3c57fd3fe7e676be22e1cbab5
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-02-09 11:39:40 +0500

    items report

--- resources/views/itemsreport.blade.php
+++ resources/views/itemsreport.blade.php
@@ -9,7 +9,6 @@
     }
     .secondary-table tr th, .secondary-table tr td{
         border: 1px solid;
-        padding: 6px;
         font-size: 14px;
     }
     .text-center{
@@ -18,15 +17,7 @@
     </style>
 </head>
 <body>
-<table cellpadding="0" cellspacing="0" style="width:100%;">
-            <tr class="text-center">
-                <td class="text-center">
-                    <h2><u>{{ $category }} Report</u></h2>
-                    <p class="font-14"><b>EFU Life Assurance Ltd.</b></p>
-                    <p class="font-14"><b>Proposed IT Budget - {{ $year }}</b></p>
-                </td>
-            </tr>
-        </table>
+
         <?php
  $grand_u_d = 0; 
  $grand_u_p = 0; 
@@ -36,27 +27,36 @@
  $grand_r = 0;
  $grand_qty = 0;
  ?> 
+ <div class="text-center">
+<h2 style="padding:0; margin:0;"><u>{{ $category }} Report</u></h2>
+                    <p style="padding:0; margin:0;" class="font-14"><b>EFU Life Assurance Ltd.</b></p>
+                    <p style="padding:0; margin:0;" class="font-14"><b>Proposed IT Budget - {{ $year }}</b></p>
+</div>
 @foreach($types as $type)   
 <div class="card mb-4 mt-3">
                             <div class="card-body">
-                            <h3><u>{{ $type->type }}</u></h3>
+                            
+                            <div class="card-body">
+                            <div class="text-center" style="border:1px solid; margin-top:10px;">
+                            <h2>{{ $type->type }}</h2>
+                            </div>
                             <span class="text-danger">{{ $errors->first('inv_id') }}</span>
                                 <div class="table-responsive">
                                     <table class="secondary-table">
                                         <thead>
                                             <tr>
                                                 <th>S.No</th>
-                                                <th>Date</th>
-                                                <th>Category</th>
                                                 <th>Item</th>
-                                                <th>Type</th>
                                                 <th>Dept</th>
-                                                <th>Remarks</th>
-                                                <th>Price Unit</th>
+                                                <th>Desc</th>
                                                 <th>Qty</th>
-                                                <th>Price Total</th>
+                                                <th>Price Unit $</th>
+                                                <th>Price Unit Rs</th>
+                                                <th>Price Total $</th>
+                                                <th>Price Total Rs</th>
                                                 <th>Consumed</th>
                                                 <th>Rem</th>
+                                                <th>Remarks</th>
                                             </tr>
                                         </thead>
                                         
@@ -74,18 +74,17 @@
                                         @foreach ($type->budgets as $budget)
                                             <tr>
                                                 <td>{{ $i++ }}</td>
-                                                <td>{{ date('Y-m-d' ,strtotime($budget->created_at)) }}</td>
-                                                <td>{{ $budget->category_id?$budget->category->category_name:'' }}</td>
                                                 <td>{{ $budget->subcategory_id?$budget->subcategory->sub_cat_name:'' }}</td>
-                                                <td>{{ $budget->type_id?$budget->type->type:'' }}</td>
                                                 <td>{{ $budget->department }}</td>
-                                                <td>{{ $budget->remarks }}</td>
-                                                <td>{{ number_format($budget->unit_price_dollar,2) }}$/<br>Rs{{ number_format($budget->unit_price_pkr,2) }}</td>
+                                                <td>{{ $budget->description }}</td>
                                                 <td>{{ $budget->qty }}</td>
-                                                <td>{{ number_format($budget->unit_price_dollar*$budget->qty,2) }}$/<br>Rs{{ number_format($budget->unit_price_pkr*$budget->qty,2) }}</td>
-                                                
+                                                <td>{{ number_format($budget->unit_price_dollar,2) }}</td>
+                                                <td>{{ number_format($budget->unit_price_pkr,2) }}</td>
+                                                <td>{{ number_format($budget->unit_price_dollar*$budget->qty,2) }}</td>
+                                                <td>{{ number_format($budget->unit_price_pkr*$budget->qty,2) }}</td>
                                                 <td>{{ $budget->consumed }}</td>
                                                 <td>{{ $budget->remaining }}</td>
+                                                <td>{{ $budget->remarks }}</td>
                                                 
                                             </tr>
                                             <?php
@@ -101,49 +100,46 @@
                                         </tbody>
                                         <tfoot>
                                             <tr>
-                                                <th colspan='7' style="text-align:right;">Total</th>
-                                                <td>{{ number_format($unit_b_d,2) }}$/<br>Rs{{ number_format($unit_b_p,2) }}</td>
+                                                <th colspan='4' style="text-align:right;">Total</th>
                                                 <td>{{ $qty }}</td>
-                                                <td>{{ number_format($total_b_d,2) }}$/<br>Rs{{ number_format($total_b_p,2) }}</td>
+                                                <td>{{ number_format($unit_b_d,2) }}</td>
+                                                <td>{{ number_format($unit_b_p,2) }}</td>
+                                                <td>{{ number_format($total_b_d,2) }}</td>
+                                                <td>{{ number_format($total_b_p,2) }}</td>
                                                 <td>{{ $t_consume }}</td>
                                                 <td>{{ $t_rem }}</td>
+                                                <td></td>
                                             </tr>
                                         </tfoot>
-                                    </table>
-                                </div>
-                            </div>
-                        </div> 
-<?php
+                                        <?php
 $grand_u_d += $unit_b_d; 
 $grand_u_p += $unit_b_p; 
 $grand_t_d += $total_b_d; 
 $grand_t_p += $total_b_p; 
 $grand_c += $t_consume; 
 $grand_r += $t_rem;
-$grand_qty = $qty;
- ?>  
-                    @endforeach
-                    <table class="secondary-table" style="margin-top:30px;">
-                                        <thead>
-                                            <tr>
-                                                <th>Grand Total</th>
-                                                <th>Price Unit</th>
-                                                <th>Qty</th>
-                                                <th>Price Total</th>
-                                                <th>Consumed</th>
-                                                <th>Rem</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody> 
-                                                <tr>
-                                                <th class="text-center">Grand Total</th>
-                                                <td>{{ number_format($grand_u_d,2) }}$/<br>Rs{{ number_format($grand_u_p,2) }}</td>
+$grand_qty += $qty;
+ ?> 
+ @if($type->type == 'Opex')
+                                         <tfoot>
+                                        <tr>
+                                                <th colspan="4" class="text-center">Grand Total</th>
                                                 <td>{{ $grand_qty }}</td>
-                                                <td>{{ number_format($grand_t_d,2) }}$/<br>Rs{{ number_format($grand_t_p,2) }}</td>
+                                                <td>{{ number_format($grand_u_d,2) }}</td>
+                                                <td>{{ number_format($grand_u_p,2) }}</td>
+                                                <td>{{ number_format($grand_t_d,2) }}</td>
+                                                <td>{{ number_format($grand_t_p,2) }}</td>
                                                 <td>{{ $grand_c }}</td>
                                                 <td>{{ $grand_r }}</td> 
+                                                <td></td>
                                                 </tr>
-                                        </tbody>   
-                    </table>            
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
+                        </div> 
+ @endif
+                    @endforeach
+                         
 </body>
 </html>
